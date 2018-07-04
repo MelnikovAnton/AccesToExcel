04de6b2b1f024456f243cc523c85aefe44146477
melnikov <melnikoffaa@mail.ru>
2018-07-04 13:42:20 +0300

    modified:   src/main/java/ru/melnikov/accesstoexcel/Main.java

--- src/main/java/ru/melnikov/accesstoexcel/Main.java
+++ src/main/java/ru/melnikov/accesstoexcel/Main.java
@@ -15,7 +15,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         log.info("Start init...");
-
         try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("app.properties")) {
             props.load(inputStream);
         }
